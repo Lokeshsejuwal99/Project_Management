d41337417c922faa1ec2fd2336131a09e273611c
Lokesh Sejuwal <sejuwallokesh84@gmail.com>
2024-05-05 12:17:52 +0545

    nth

--- apps/Project/admin.py
+++ apps/Project/admin.py
@@ -1,7 +1,3 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# # Register your models here.
-
-
-for i in range(2):
- print("Dikshya <3 <3" * 80)
+# Register your models here.
