486d3256b8a8b957120ed1fef9c8a90942584132
Lokesh Sejuwal <sejuwallokesh84@gmail.com>
2024-05-18 02:17:27 +0545

    flake

--- apps/Task/__init__.py
+++ apps/Task/__init__.py
@@ -13,9 +13,25 @@
 # check = result(10)
 # print(check)
 
-def make_divide(x):
- def divide(div):
-  return x / div
- return divide
-check_divide = make_divide(15)
-print(check_divide(5))
+# def make_divide(x):
+#  def divide(div):
+#   return x / div
+#  return divide
+# check_divide = make_divide(15)
+# print(check_divide(5))
+
+# def example(*args):
+#  return sum(args)
+# smth = example(1,2,3,4)
+# print(smth)
+
+
+# def kwars(**kwargs):
+#  print(kwargs)
+
+# kwars(a=1, b=2, c=3)
+
+def check_kwargs(**kwargs):
+ for key, value in kwargs.items():
+  print(f'{key} = {value}')
+check_kwargs(name='lokesh', age=21, city='KTM')
